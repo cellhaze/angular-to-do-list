081bea30a914eb3e7a284d3f376a543a91db6c4f
Marcell Hayes <marcell.made@gmail.com>
2017-05-02 09:04:48 -0400

    push

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ app.controller('myController', function($scope) {
     
     $scope.addTask = function(newTask) {
         
-        console.log(newTask);
+//        console.log(newTask);
+        $scope.toDoList.push({ task: newTask });
+        
+        console.log($scope.toDoList);
         
     };
-    
+
+$scope.removeItem = function(i) {
+    $scope.toDoList.splice(i, 1);
+}; 
     
     
     
